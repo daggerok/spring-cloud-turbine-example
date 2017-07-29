4d25293a7754a7f56d275b49ebcd2bdc94b75014
Maksim Kostromin <daggerok@gmail.com>
2017-07-29 20:29:52 +0300

    Update project build.

--- infrastructure/hystrix-aggregator/src/main/java/daggerok/Main.java
+++ infrastructure/hystrix-aggregator/src/main/java/daggerok/Main.java
@@ -2,7 +2,6 @@ package daggerok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
--- infrastructure/hystrix-dashboard/src/main/java/daggerok/Main.java
+++ infrastructure/hystrix-dashboard/src/main/java/daggerok/Main.java
@@ -2,7 +2,6 @@ package daggerok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 public class Main {
