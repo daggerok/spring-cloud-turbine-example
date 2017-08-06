f12d5e87fad14d44481568e587cc838f41582e65
Maksim Kostromin <daggerok@gmail.com>
2017-08-06 03:09:26 +0300

    Fix turbine. Add asciidoctor.

--- infrastructure/hystrix-aggregator/src/main/java/daggerok/Main.java
+++ infrastructure/hystrix-aggregator/src/main/java/daggerok/Main.java
@@ -2,11 +2,14 @@ package daggerok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
 @EnableHystrixDashboard
-@SpringCloudApplication
 public class Main {
 
   public static void main(String[] args) {
